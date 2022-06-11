08c6f426dbe1674ed1e6fa43c5ba7a7a362328ac
michal2467300 <michal2467300@gmail.com>
2022-06-11 23:25:48 +0300

    game service

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import { WebService } from './web.service';
+import Game from '../models/game';
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
-  constructor() { }
+  constructor(private WebService: WebService) { }
+
+   GetGameBySize(width: number, height: number) : Game {
+    return {game: "*-----**-----",
+            size: {width: 4,
+                  height: 3}}
+  }
 }
