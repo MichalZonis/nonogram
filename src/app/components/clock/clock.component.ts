e6fc16727e513904ad77ca32e2028ef63246c6ff
Michal Zonis <michal2467300@gmail.com>
2022-07-01 18:49:32 +0300

    clock compoment 1.7.22

---
+++ src/app/components/clock/clock.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Subscription, timer } from 'rxjs';
+import { map, share } from "rxjs/operators";
+import { OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-clock',
+  templateUrl: './clock.component.html',
+  styleUrls: ['./clock.component.css']
+})
+export class ClockComponent implements OnDestroy, OnInit {
+  
+  counter: number = 0;
+  timerRef!: any;
+  running: boolean = false;
+
+  ngOnInit() {
+    this.startTimer();
+  }
+
+  startTimer() {
+    this.running = true;
+      const startTime = Date.now();
+      this.timerRef = setInterval(() => {
+        this.counter = Date.now() - startTime;
+        console.log(this.counter)
+      }, 1000)
+  }
+
+  clearTimer() {
+    this.running = false;
+    this.counter = 0;
+    clearInterval(this.timerRef);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timerRef);
+  }
+
+}
