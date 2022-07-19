21ab1bcb8c8737665cf78782df6e01287f007e38
Michal Zonis <michal2467300@gmail.com>
2022-07-20 00:21:50 +0300

    new compponants added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,31 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './components/board/board.component';
 import { GameComponent } from './components/game/game.component';
+import { HintsComponent } from './components/hints/hints.component';
+import { ClockComponent } from './components/clock/clock.component';
+import { AppRoutingModule } from './app-routing.module';
+import { GameCreationComponent } from './components/game-creation/game-creation.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BoardComponent,
-    GameComponent
+    GameComponent,
+    HintsComponent,
+    ClockComponent,
+    GameCreationComponent,
+    LoginComponent,
+    SignInComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
