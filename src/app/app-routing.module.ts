d23b8418a6bef3f6e8d784f880e4518e05234e52
Michal Zonis <michal2467300@gmail.com>
2022-08-12 22:40:18 +0300

    authenticated navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,17 @@ import { GameComponent } from './components/game/game.component';
 import {GameCreationComponent} from './components/game-creation/game-creation.component'
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'play', component: GameComponent },
   { path: 'create', component: GameCreationComponent},
   { path: 'login', component: LoginComponent},
+  { path: 'register', component: SignInComponent},
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuardService]},
   { path: 'home', component: HomeComponent}
 
 ];
