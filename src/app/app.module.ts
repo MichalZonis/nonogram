8bb7032ce9fc9b8e96e743fed1cb2615e1bf90b1
Michal Zonis <michal2467300@gmail.com>
2022-08-12 22:39:55 +0300

    profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { GameCreationComponent } from './components/game-creation/game-creation.
 import { LoginComponent } from './components/login/login.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { HomeComponent } from './components/home/home.component';
     GameCreationComponent,
     LoginComponent,
     SignInComponent,
-    HomeComponent
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
