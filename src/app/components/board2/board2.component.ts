51a9372bd4f56641de80d9575a9f4541a87e3882
Michal Zonis <michal2467300@gmail.com>
2023-08-12 18:17:32 +0300

    refactored board comp

--- src/app/components/board2/board2.component.ts
+++ src/app/components/board2/board2.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { states } from 'src/app/models/states.enum';
 
 @Component({
   selector: 'app-board2',
@@ -7,22 +8,68 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 export class Board2Component implements OnInit {
 
-  rows = new Array<number>(3);
-  cols = new Array<number>(6);
+  @Input() rows = 4;
+  @Input() cols = 16;
+  gridState: states[][] = new Array(this.rows).fill("").map(() => new Array(this.cols).fill(states.undecided))
+  states = states;
+  @Input() BoardSeq: string = "-*-*-*****-***---*--"
+  @Input() parentWidth: number = window.innerWidth
+  @Input() isReadOnly = false;
 
-  dynamicWidth =  `calc(100% / ${this.cols.length})`
-  @ViewChild('table') grid!: ElementRef;
-
+  cell_width!: number;
 
   constructor() { }
 
   ngOnInit(): void {
-    console.log(this.dynamicWidth)
+    this.calcCellMeasures()
+    this.populateGrid();
   }
-  
-  ngAfterViewInit() {
-    console.log(this.grid.nativeElement.offsetWidth)
-    console.log(window.innerWidth)
 
+  calcCellMeasures() {
+    const max_width_table = this.parentWidth * 100 / 100;
+    this.cell_width = 30;
+    if (this.cols * this.cell_width > max_width_table) {
+      this.cell_width = max_width_table / this.cols
+    }
   }
+
+  ngOnChanges(changes: SimpleChanges) {
+    //console.log(changes)
+    if (changes["parentWidth"])
+      this.calcCellMeasures()
+  }
+
+  populateGrid() {
+    let tmpGrid: states[][] = new Array(this.rows).fill("").map(() => new Array(this.cols).fill(states.undecided))
+
+    // if the board need to be initialized according to a sequence then blacken the cells accordingly
+    if (this.BoardSeq) {
+      for (let i = 0; i < this.rows; i++) {
+        for (let j = 0; j < this.cols; j++) {
+          if (this.BoardSeq.charAt(this.cols * i + j) == states.black) {
+            tmpGrid[i][j] = states.black;
+          }
+        }
+      }
+    }
+
+    this.gridState = tmpGrid
+  }
+
+  forwardToggle(row: number, col: number) {
+    let enumVals = Object.values(states)
+    let index = enumVals.indexOf(this.gridState[row][col])
+    let next = enumVals[(index + 1) % 3]
+    this.gridState[row][col] = next
+  }
+
+  backwardToggle(row: number, col: number) {
+    let enumVals = Object.values(states)
+    let index = enumVals.indexOf(this.gridState[row][col])
+    let next = enumVals[(index + 2) % 3]
+    this.gridState[row][col] = next
+    return false;
+  }
+
+  
 }
